bae305464dffbf6a83eaa9f28e32c692d64e3601
afra666 <48674984+afra666@users.noreply.github.com>
2019-10-13 20:56:10 +0800

    Update 006.fs方法.js

--- 006.fs方法.js
+++ 006.fs方法.js
@@ -1,3 +1,4 @@
+//异步
 let fs=require('fs');
 fs.readFile('my.txt',function (err,data) {
     if (err){
@@ -10,3 +11,16 @@ fs.readFile('my.txt',function (err,data) {
  this is from my.txt
  这是来自文本的文字
  */
+
+
+
+=================================
+
+
+//同步读取，占用IO
+
+//同步读取文件fs
+let fs=require('fs');
+let data=fs.readFileSync('my.txt');
+console.log(data.toString());
+console.log("同步读取成功 ！");
