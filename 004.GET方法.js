868343ad8c0bcb2e0de382123d5ae8114f7a667c
afra666 <48674984+afra666@users.noreply.github.com>
2019-10-13 20:17:00 +0800

    Create 004.GET方法.js

---
+++ 004.GET方法.js
@@ -0,0 +1,16 @@
+//express_demo.js 文件
+var express = require('express');//加载express
+var app = express();//实例化一个express应用
+
+
+app.get('/ts', function (req, res) {
+  res.send('Hello World123466666');
+});//在express上监听get请求
+
+app.listen(8888);//添加端口
+
+
+==========================out===================
+http://localhost:8888/ts
+
+Hello World123466666
