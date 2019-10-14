1393cad781fb79fc0a18ef163c3cfe7a997ac5b7
afra666 <48674984+afra666@users.noreply.github.com>
2019-10-14 18:34:29 +0800

    Update 025.读取文件.js

--- 025.读取文件.js
+++ 025.读取文件.js
@@ -2,3 +2,8 @@ let fs=require('fs');
 fs.readFile('./a.txt',(err,data)=>{
     console.log(data.toString());
 })
+//
+let fs=require('fs');
+fs.readFile('./a.txt','utf8',(err,data)=>{
+    console.log(data);
+})
