6eb21426ebf3c33578a60d12c3804056fd9df2ea
afra666 <48674984+afra666@users.noreply.github.com>
2019-10-14 18:11:58 +0800

    Update 021.path.js

--- 021.path.js
+++ 021.path.js
@@ -4,3 +4,10 @@ console.log(__dirname);
 
 //D:\Program Files\jqm
 console.log(path.dirname(__dirname));
+
+================================================================
+let path=require('path');
+
+console.log(path.extname('/sgsd/sfdgfs/b.html'));
+
+//.html
